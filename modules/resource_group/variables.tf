e03e14d2957d595131dfdb0443d1b7fab15babb7
AshaShreeDK <dlrashadk@gmail.com>
2025-10-08 21:58:15 +0530

    Create resource group module: variables (app name, location)

---
+++ modules/resource_group/variables.tf
@@ -0,0 +1,9 @@
+variable "application_name" {
+  type        = string
+  description = "Base name for all resources"
+}
+
+variable "location" {
+  type        = string
+  description = "Azure region for the resource group"
+}
