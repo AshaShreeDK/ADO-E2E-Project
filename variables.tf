5604bdabb891a1038b3371795f3967ef65e6dd44
AshaShreeDK <dlrashadk@gmail.com>
2025-10-08 21:13:50 +0530

    Create root variables for Terraform modules and resources

---
+++ variables.tf
@@ -0,0 +1,40 @@
+variable "application_name" {
+  type        = string
+  description = "Base name for all resources."
+}
+
+variable "location" {
+  type        = string
+  description = "Azure region (e.g., australiaeast)."
+}
+
+variable "java_cidr" {
+  type        = list(string)
+  description = "VNet address space."
+}
+
+variable "java_sub_cidr" {
+  type        = list(string)
+  description = "Subnet address space."
+}
+
+variable "vm_size" {
+  type        = string
+  description = "Azure VM size."
+}
+
+variable "vm_username" {
+  type        = string
+  description = "VM admin username."
+}
+
+variable "vm_pass" {
+  type        = string
+  description = "VM admin password."
+  sensitive   = true
+}
+
+variable "ssh_public_key_path" {
+  type        = string
+  description = "Path to SSH public key file (OpenSSH format)"
+}
